e272b396e725b660d66d37691c75f46b5ef3de08
jorgerojas26 <jorgeluisrojasb@gmail.com>
2019-05-16 01:46:11 -0400

    solving basic problems

--- database.js
+++ database.js
@@ -2,14 +2,15 @@ var mongoose = require('mongoose');
 
 mongoose.set('useFindAndModify', false);
 
-mongoose.connect('mongodb://localhost/todoDB', {
-    useNewUrlParser: true
-});
-/*
-mongoose.connect("mongodb+srv://jorgerojas26:jorgerojas26@cluster0-q4dxm.mongodb.net/test?retryWrites=true", {
+/*mongoose.connect('mongodb://localhost/todoDB', {
     useNewUrlParser: true
 });
 */
+
+mongoose.connect("mongodb+srv://jorgerojas26:jorgerojas26@cluster0-q4dxm.mongodb.net/test?retryWrites=true", {
+    useNewUrlParser: true
+});
+
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'database connection error:'));
